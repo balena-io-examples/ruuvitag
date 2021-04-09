dd3d14fc976c2af0ff5b35e23ec028cc395e0059
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2021-04-09 17:16:20 +0300

    Publish mac address of the sensor
    MAC address allows data to be separated based on sensor

--- sensor/main.py
+++ sensor/main.py
@@ -1,11 +1,15 @@
+
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 import paho.mqtt.client as mqtt
 import json
 
 def handle_data(found_data):
+    merged = {"mac": found_data[0]}
+    merged.update(found_data[1])
+
     client.connect("localhost")
-    print(found_data[1])
-    msgInfo = client.publish("sensors", json.dumps(found_data[1]))
+    print(merged)
+    msgInfo = client.publish("sensors", json.dumps(merged))
     if False == msgInfo.is_published():
         msgInfo.wait_for_publish()
     client.disconnect()
